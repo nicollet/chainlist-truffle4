0cef2af57616cfe4bf57f73cdc293e62968c78d8
Xavier Nicollet <xnicollet@gmail.com>
2021-06-28 21:50:46 +0100

    wip

--- startrinkeby-linux.sh
+++ startrinkeby-linux.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "~/.ethereum/geth.ipc"
+geth --rinkeby --syncmode "light" --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "~/.ethereum/geth.ipc" --cache=1024
